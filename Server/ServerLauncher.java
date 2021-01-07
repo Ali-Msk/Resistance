e9c4c714291ab67ed840e53a7d6dc74eb6d93254
Ali Meshkat <seyedali.meshkat@gmail.com>
2021-01-07 11:18:02 -0500

    Add files via upload

---
+++ Server/ServerLauncher.java
@@ -0,0 +1,12 @@
+/**
+ * [ServerLauncher.java]
+ * is the launcher of the server of Resistance
+ * @author Ali Meshkat
+ */
+
+
+public class ServerLauncher {
+ public static void main(String[] args){
+  new ServerConnector();
+ }
+}
